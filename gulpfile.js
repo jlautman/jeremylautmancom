e00d8fccc9d731755eaeb3b2739de0eccab519d5
jlautman <jlautman@seas.upenn.edu>
2017-08-28 11:58:46 -0400

    First draft completed

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,37 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat');
 
-var jsSources = ['scripts/*.js'],
-    sassSources = ['styles/*.scss'],
-    htmlSources = ['**/*.html'],
-    outputDir = 'public';
+var jsDir = 'scripts/';
+var sassDir = 'styles/';
+var htmlDir = 'html/';
+var outputDir = 'target/';
+
+var jsSources = jsDir + "**/*.js";
+var sassSources = sassDir + "**/*.scss";
+var htmlSources = htmlDir + "**/*.html";
 
 
-gulp.task('log', function() {
-  gutil.log('== My First Task ==')
-});
-
-gulp.task('copy', function() {
-  gulp.src('index.html')
+gulp.task('html', function() {
+  gulp.src(htmlSources)
   .pipe(gulp.dest(outputDir))
+  .pipe(connect.reload());
 });
 
 gulp.task('sass', function() {
   gulp.src(sassSources)
   .pipe(sass({style: 'expanded'}))
     .on('error', gutil.log)
-  .pipe(gulp.dest(outputDir))
+  .pipe(concat('main.css'))
+  .pipe(gulp.dest(outputDir + sassDir))
   .pipe(connect.reload())
 });
 
 gulp.task('js', function() {
   gulp.src(jsSources)
   .pipe(uglify())
-  .pipe(concat('script.js'))
-  .pipe(gulp.dest(outputDir))
-  .pipe(connect.reload())
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest(outputDir + jsDir))
+  .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
@@ -44,14 +46,9 @@ gulp.task('watch', function() {
 
 gulp.task('connect', function() {
   connect.server({
-    root: '.',
+    root: 'target',
     livereload: true
-  })
-});
-
-gulp.task('html', function() {
-  gulp.src(htmlSources)
-  .pipe(connect.reload())
+  });
 });
 
 gulp.task('default', ['html', 'js', 'sass', 'connect', 'watch']);
